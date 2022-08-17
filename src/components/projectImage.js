d6733e243b430abf00f2ea6bfac6539d1b2be270
matt <mattwjames@msn.com>
2022-08-16 20:09:57 -0600

    added onMouseEnter onMouseLeave

--- src/components/projectImage.js
+++ src/components/projectImage.js
@@ -1,15 +1,19 @@
 import { Image } from "@chakra-ui/react";
+import {useState} from "react";
 
 const ProjectImage = ({ img, desc }) => {
+    const [imgWidth, setImgWidth] = useState(600)
     return (
         <>
             <Image
                 className="image"
                 borderRadius="lg"
-                width={{ md: 80, lg: 600 }}
+                width={{ md: 80, lg: imgWidth }}
                 src={img}
                 alt={desc}
                 border="solid"
+                onMouseEnter={()=>{ setImgWidth(1000)}}
+                onMouseLeave={()=>{ setImgWidth(600)}}
                 borderColor="gray.300"
             />
         </>
